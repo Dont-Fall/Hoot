132f16bfd247b8fa2f44d66ec2a4f14b146a0ac7
Julasevich <ulasevich.jacob@gmail.com>
2016-01-11 16:58:46 -0500

    JakeCommit_1/11_4:58

--- Hoot/QuestionAnswerViewController.swift
+++ Hoot/QuestionAnswerViewController.swift
@@ -10,17 +10,72 @@ import UIKit
 import Parse
 import ParseUI
 
-class QuestionAnswerViewController: UIViewController {
+class QuestionAnswerViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
 
     @IBOutlet var questionAnswerAnswerTV: UITextView!
     @IBOutlet var questionAnswerPicPreview: UIImageView!
     @IBOutlet var questionAnswerAnswerCount: UILabel!
 
     @IBAction func questionAnswerAddPicBtn(sender: AnyObject) {
+        var imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            
+            imagePicker.sourceType = .Camera
+            if (UIImagePickerController.isCameraDeviceAvailable(.Front)) { imagePicker.cameraDevice = .Front
+            }else{
+                imagePicker.cameraDevice = .Rear
+            }
+            
+        }else{
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        self.presentViewController(imagePicker, animated: true, completion: nil)
     }
+
+
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
+        questionAnswerPicPreview.image = image; self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+    // Container to store the view table selected object
+    var questionID : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        questionAnswerPicPreview.layer.cornerRadius = 8.0
+        questionAnswerPicPreview.clipsToBounds = true
+        // Do any additional setup after loading the view.
+        
+        //Keyboard up at start
+        questionAnswerAnswerTV.becomeFirstResponder()
+        
+        //Live Count Delegate
+        self.questionAnswerAnswerCount.text = "200"
+        self.questionAnswerAnswerTV.delegate = self
+        
+        questionAnswerAnswerTV.textInputView.needsUpdateConstraints()
+        questionAnswerAnswerTV.textInputView.sizeToFit()
+        
+        //MARK: Nav Bar Customize
+        let answerQuestionCancelBtn = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: "answerQuestionCancel")
+        let answerQuestionAnswerBtn = UIBarButtonItem(title: "Answer", style: .Plain, target: self, action: "answerQuestionAnswerQuestion")
+        self.navigationItem.setRightBarButtonItem(answerQuestionAnswerBtn, animated: true)
+        self.navigationItem.setLeftBarButtonItem(answerQuestionCancelBtn, animated: true)
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UIBarButtonItem.appearance().tintColor = UIColor.whiteColor()
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.systemFontOfSize(14.0)], forState: UIControlState.Normal)
+        answerQuestionAnswerBtn.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.systemFontOfSize(14.0)], forState: UIControlState.Normal)
+        self.title = "Answer Question"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        //Ask Button Activation
+        answerQuestionAnswerBtn.enabled = true
+        
+        //Start TV at Top Left
+        questionAnswerAnswerTV.addObserver(self, forKeyPath: "contentSize", options: NSKeyValueObservingOptions.New, context: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -28,6 +83,47 @@ class QuestionAnswerViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Go Back Function
+    func answerQuestionCancel() {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    //Answer Question FUnction
+    func answerQuestionAnswerQuestion() {
+        var currentUser = PFUser.currentUser()
+        var answer = PFObject(className: "Answer")
+        answer["question"] = questionAnswerAnswerTV.text
+        answer["user"] = currentUser!.username
+        answer["correct"] = false
+        answer["idNumber"] = questionID
+        answer["school"] = currentUser!.objectForKey("school")
+        answer["subject"] = currentUser!.objectForKey("subject")
+        answer["reportNumber"] = 0
+        answer["reported"] = false
+        //PIC
+        let imageData = UIImageJPEGRepresentation(self.questionAnswerPicPreview.image!,0.5)
+        let imageFile = PFFile(name:"image.jpeg", data:imageData!)
+        answer.setObject(imageFile!, forKey: "picture")
+        //MARK: Save Question
+        answer.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if error == nil {
+                print("Yp")
+                // Success, no creating error
+                currentUser!.incrementKey("points", byAmount: 5)
+                currentUser!.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                    if error == nil {
+                        print("Points Updated")
+                    } else {
+                        print("Error")
+                    }
+                }
+                self.navigationController?.popViewControllerAnimated(true)
+            } else {
+                print("Error")
+            }
+        }
     }
     
 
