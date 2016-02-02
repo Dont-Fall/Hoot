5067c7a2c433a38548ea4033a88ac5b38c23b2f5
Julasevich <ulasevich.jacob@gmail.com>
2016-02-02 15:38:38 -0500

    Jake_2/2/16_3:38

--- Hoot/QuestionAnswerViewController.swift
+++ Hoot/QuestionAnswerViewController.swift
@@ -132,16 +132,5 @@ class QuestionAnswerViewController: UIViewController, UITextFieldDelegate, UIIma
             }
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
