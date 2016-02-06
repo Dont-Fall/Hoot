40821c68949ea0fa76f70423b1722d3a65b94262
Zack <zack.cuomo@gmail.com>
2016-02-06 13:02:55 -0500

    ZackCuomo_2/6_1:02_CreatedClassesForErrorsUF

---
+++ Hoot/AddSchool.swift
@@ -0,0 +1,60 @@
+//
+//  AskQuestion.swift
+//  Hoot
+//
+//  Created by Zack Cuomo on 1/15/16.
+//  Copyright © 2016 Nitrox Development. All rights reserved.
+
+import Foundation
+
+class AddSchool: NSObject {
+    
+    var userEvent: String?
+    var userLocation: String?
+    var eventDate: String?
+//    let alert = UIAlertController()
+    
+    init(event: String, location: String, date: String){
+        self.userEvent = event
+        self.userLocation = location
+        self.eventDate = date
+    
+    }
+    
+    func hasNoEmptyFields() -> Bool{
+        if !userEvent!.isEmpty && !userLocation!.isEmpty && !eventDate!.isEmpty{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func eventSufficientLength() -> Bool{
+        if userEvent!.characters.count < 5{
+            return false
+        }else{
+            return true
+        }
+    }
+    
+    func locationSufficientLenght() -> Bool{
+        if userLocation!.characters.count < 3{
+            return false
+        }else{
+            return true
+        }
+    }
+
+    
+    func addSchoolAlert() throws {
+        guard hasNoEmptyFields() else{
+            throw Error.EmptyField
+        }
+        guard eventSufficientLength() else{
+            throw Error.InsufficientEventLength
+        }
+        guard locationSufficientLenght() else{
+            throw Error.InsufficientLocationLength
+        }
+    }
+}
---
+++ Hoot/ClassQuestionAnswer.swift
@@ -0,0 +1,59 @@
+//
+//  AskQuestion.swift
+//  Hoot
+//
+//  Created by Zack Cuomo on 1/15/16.
+//  Copyright © 2016 Nitrox Development. All rights reserved.
+
+import Foundation
+
+class ClassQuestionAnswer: NSObject {
+    
+    var userTopic: String?
+    var userText: String?
+    var image: PFFile?
+//    let alert = UIAlertController()
+    
+    init(topic: String, text: String, img: PFFile){
+        self.userTopic = topic
+        self.userText = text
+        self.image = img
+    }
+    
+    func hasNoEmptyFields() -> Bool{
+        if !userTopic!.isEmpty && !userText!.isEmpty{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func topicSufficientLength() -> Bool{
+        if userTopic!.characters.count < 4{
+            return false
+        }else{
+            return true
+        }
+    }
+    
+    func questionSufficientLenght() -> Bool{
+        if userText!.characters.count > 200{
+            return false
+        }else{
+            return true
+        }
+    }
+
+    
+    func classQuestionAnswerAlert() throws {
+        guard hasNoEmptyFields() else{
+            throw Error.EmptyField
+        }
+        guard topicSufficientLength() else{
+            throw Error.InsufficientTopicLength
+        }
+        guard questionSufficientLenght() else{
+            throw Error.InsufficientQuestionLength
+        }
+    }
+}
